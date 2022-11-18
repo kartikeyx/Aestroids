b83174b07ee5a5d29aed5d9eb87e1c4c4b1f02db
Kartikey <75428247+kartikeyx@users.noreply.github.com>
2022-11-18 15:55:38 +0530

    Initial Commit

--- core/src/com/k3k/game/Asteroids.java
+++ core/src/com/k3k/game/Asteroids.java
@@ -1,19 +1,27 @@
 package com.k3k.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-public class Aestroids extends ApplicationListener {
+public class Asteroids implements ApplicationListener {
+	public static int WIDTH;
+	public static int HEIGHT;
+
+	public static OrthographicCamera cam;
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
+		WIDTH = Gdx.graphics.getWidth();
+		HEIGHT = Gdx.graphics.getHeight();
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		cam = new OrthographicCamera();
 	}
 
 	@Override
