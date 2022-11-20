da51f709e38e582bcaf656b7b6619f4f2cebf7e7
Kartikey <75428247+kartikeyx@users.noreply.github.com>
2022-11-20 14:14:38 +0530

    :

--- core/src/com/k3k/game/Asteroids.java
+++ core/src/com/k3k/game/Asteroids.java
@@ -1,7 +1,10 @@
 package com.k3k.game;
 
+import cam.k3k.managers.GameInputProcessor;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -21,7 +24,13 @@ public class Asteroids implements ApplicationListener {
 		HEIGHT = Gdx.graphics.getHeight();
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
-		cam = new OrthographicCamera();
+		cam = new OrthographicCamera(WIDTH, HEIGHT);
+		cam.translate(WIDTH/2, HEIGHT/2);
+		cam.update();
+
+		Gdx.input.setInputProcessor(
+				new GameInputProcessor()
+		);
 	}
 
 	@Override
@@ -31,7 +40,12 @@ public class Asteroids implements ApplicationListener {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0,0,0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+//		if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+//			System.out.println("SPACE");
+//		}
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
